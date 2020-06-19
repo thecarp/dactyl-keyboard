thecarp/dactyl-keyboard

commit 1c2d6938e452b68150742a5a49200331df1227b9

Key-mount wall thickness by key.
Changed default tweak size to nodule, relegating the previous default
case to key mounts only.

Moved parameters controlling key-mount corner posts into the by-key
section, thus reinstating wall thickness with many more uses.

Added a `thickness` parameter to rear-housing shape, replacing the
use of key-mount posts there.

Added a `backing-thickness` parameter for MCU lock plates, replacing key
webbing thickness there.

Removed the addition of key webbing thickness from the design of a key
switch negatives, which can lead to thick key mounts having no
through-holes.

Refactored key webbing functions.

src/dactyl_keyboard/cad/place.clj
@@ -39,19 +39,23 @@
 
 ;; Key mounts.
 
-(defn mount-corner-offset
+(defn- mount-corner-offset
   "Produce a mm coordinate offset for a corner of a switch mount.
   This is not to be confused with offsets for walls, which are additive."
-  [getopt key-style side]
+  [getopt cluster coord side]
   {:pre [(or (nil? side) (side compass/all-short))]}
-  (let [style-data (getopt :keys :derived key-style)
+  (let [specific-side (if (nil? side)
+                        :dactyl-keyboard.cad.key/any
+                        (compass/short-to-long (compass/convert-to-cardinal side)))
+        directions (get compass/keyword-to-tuple side (if side [side] []))
+        most #(most-specific getopt %& cluster coord specific-side)
+        style-data (getopt :keys :derived (most :key-style))
         [subject-x subject-y] (map measure/key-length
                                    (get style-data :unit-size [1 1]))
-        m (getopt :main-body :key-mount-corner-margin)
-        directions (get compass/keyword-to-tuple side (if side [side] []))]
-    [(* (apply compass/delta-x directions) (- (/ subject-x 2) (/ m 2)))
-     (* (apply compass/delta-y directions) (- (/ subject-y 2) (/ m 2)))
-     (/ (getopt :main-body :web-thickness) -2)]))
+        [wall-x wall-y wall-z] (map #(/ % 2) (most :wall :thickness))]
+    [(* (apply compass/delta-x directions) (- (/ subject-x 2) wall-x))
+     (* (apply compass/delta-y directions) (- (/ subject-y 2) wall-y))
+     (- wall-z)]))
 
 (defn- curver
   "Given an angle for progressive curvature, apply it. Else lay keys out flat."
@@ -178,13 +182,6 @@
       (update 0 (partial * dx))
       (update 1 (partial * dy)))))
 
-(defn- wall-vertex-offset
-  "Compute a 3D offset from the center of a web post to a vertex on it."
-  [getopt side keyopts]
-  (let [xy (/ (getopt :main-body :key-mount-corner-margin) 2)
-        z (/ (getopt :main-body :key-mount-thickness) 2)]
-    (matrix/cube-vertex-offset side [xy xy z] keyopts)))
-
 (defn wall-corner-offset
   "Combined [x y z] offset from the center of a switch mount.
   This can go to one corner of the hem of the mount’s skirt of
@@ -192,11 +189,17 @@
   [getopt cluster coordinates
    {:keys [side segment vertex] :or {vertex false} :as keyopts}]
   {:pre [(or (nil? side) (compass/all-short side))]}
-  (let [key-style (most-specific getopt [:key-style] cluster coordinates)]
-    (mapv +
-      (mount-corner-offset getopt key-style side)
-      (wall-segment-offset getopt cluster coordinates side segment)
-      (if (and side vertex) (wall-vertex-offset getopt side keyopts) [0 0 0]))))
+  (mapv +
+    (mount-corner-offset getopt cluster coordinates side)
+    (wall-segment-offset getopt cluster coordinates side segment)
+    (if (and side vertex)
+      ;; Compute a 3D offset from the center of a web post to a vertex on it.
+      (matrix/cube-vertex-offset
+        side
+        (most-specific getopt [:wall :thickness] cluster coordinates
+          (compass/short-to-long (compass/convert-to-cardinal side)))
+        keyopts)
+      [0 0 0])))
 
 (defn wall-corner-place
   "Absolute position of the lower wall around a key mount."

config/base.yaml
@@ -14,12 +14,10 @@ by-key:
       height: 20
       top-width: 10
     wall:
+      thickness: 2
       bevel: 1
       perpendicular: -8
 main-body:
-  key-mount-corner-margin: 2
-  key-mount-thickness: 2
-  web-thickness: 2
   rear-housing:
     thickness:
       walls: 2.5

config/concertina/base.yaml
@@ -401,6 +401,7 @@ central-housing:
   include: true
   shape:
     width: 200
+    thickness: 2
     interface:
     # The near side, ascending:
     - above-ground: true

config/dactyl_manuform/base.yaml
@@ -89,6 +89,7 @@ by-key:
           column: 1.5
           row: 1.2
     wall:
+      thickness: [3, 3, 4]
       parallel: 5
       perpendicular: -15
   clusters:
@@ -249,9 +250,6 @@ secondaries:
     override: [null, null, 3]
 main-body:
   reflect: true
-  key-mount-thickness: 4
-  key-mount-corner-margin: 1.5
-  web-thickness: 3.5
   bottom-plate:
     include: true
     installation:

config/dmote/base.yaml
@@ -831,6 +831,8 @@ mcu:
   support:
     lock:
       include: true
+      plate:
+        backing-thickness: 3
 ports:
   # A port in the top far-right corner of the rear housing. This is for running
   # the wire between the two halves of the keyboard on or through a connecting rod

config/macropad/base.yaml
@@ -61,3 +61,5 @@ mcu:
   support:
     lock:
       include: true
+      plate:
+        backing-thickness: 3

src/dactyl_keyboard/cad/body/central.clj
@@ -5,7 +5,8 @@
 
 ;;; The central housing is one of the predefined bodies of the application.
 ;;; It is distinguished by its shape, uniquely defined in terms of a hollow
-;;; polyhedral block extending between two reflected main bodies.
+;;; polyhedral block extending between two copies of the main body, one of
+;;; these copies being reflected.
 
 (ns dactyl-keyboard.cad.body.central
   (:require [clojure.spec.alpha :as spec]
@@ -70,7 +71,7 @@
 
 ;; Predicates for filtering items in the interface for drawing different
 ;; bodies.
-(defn- above-ground?  ; If true, to be included in main body.
+(defn- above-ground?  ; If true, to be included in body.
   [{:keys [above-ground] :as point}]
   (if (nil? above-ground) (not (neg? (get-z-offset point))) above-ground))
 (defn- at-ground?  ; If true, to be included in bottom plate.
@@ -100,12 +101,13 @@
 (defn- single-receiver
   "An extension through the central-housing interface array to receive a single
   fastener. This design is a bit rough; more parameters would be needed to
-  account for the possibility of wall surfaces angled on the x or y axes."
+  account for the possibility of wall surfaces angled on the x or y axes.
+  Key-cluster wall thickness is not taken into account."
   [getopt {:keys [lateral-offset]}]
   (let [rprop (partial getopt :central-housing :adapter :receivers)
         fprop (partial getopt :central-housing :adapter :fasteners)
         diameter (fprop :bolt-properties :m-diameter)
-        z-wall (getopt :main-body :web-thickness)
+        z-wall (getopt :central-housing :shape :thickness)
         width (+ diameter (* 2 (rprop :thickness :rim)))
         z-hole (- (iso/bolt-length (fprop :bolt-properties)) z-wall)
         z-bridge (min z-hole (rprop :thickness :bridge))
@@ -191,10 +193,10 @@
     interface))
 
 (defn- locate-above-ground-points
-  "Derive 3D coordinates on the main body of the central housing."
+  "Derive 3D coordinates on the body of the central housing."
   [getopt interface]
   (let [[half-width _] (get-widths getopt)
-        thickness (getopt :main-body :web-thickness)
+        thickness (getopt :central-housing :shape :thickness)
         [cross-indexed items] (index-map interface :above-ground)
         [base-offsets _] (get-offsets items)
         [right-gabel-outer adapter-outer] (resolve-offsets getopt items)

src/dactyl_keyboard/cad/key.clj
@@ -232,27 +232,13 @@
 
 (defn- single-plate
   "The shape of a key mounting plate."
-  [getopt key-style]
-  {:pre [(keyword? key-style)]}
-  (let [thickness (getopt :main-body :key-mount-thickness)
-        style-data (getopt :keys :derived key-style)
-        [x y] (map measure/key-length (get style-data :unit-size [1 1]))]
-   (model/translate [0 0 (/ thickness -2)]
-     (model/cube x y thickness))))
-
-(defn web-post
-  "A shape for attaching things to a corner of a switch mount."
-  [getopt]
-  (model/cube (getopt :main-body :key-mount-corner-margin)
-              (getopt :main-body :key-mount-corner-margin)
-              (getopt :main-body :web-thickness)))
-
-(defn mount-corner-post
-  "A post shape that comes offset for one corner of a key mount."
-  [getopt key-style side]
-  {:pre [(compass/intermediates side)]}
-  (->> (web-post getopt)
-       (model/translate (place/mount-corner-offset getopt key-style side))))
+  [getopt cluster coord]
+  (let [most #(most-specific getopt % cluster coord)
+        style-data (getopt :keys :derived (most [:key-style]))
+        [x y] (map measure/key-length (get style-data :unit-size [1 1]))
+        z (most [:wall :thickness 2])]
+    (model/translate [0 0 (/ z -2)]
+      (model/cube x y z))))
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -262,7 +248,7 @@
 (defn cluster-plates [getopt cluster]
   (apply model/union
     (map #(place/cluster-place getopt cluster %
-            (single-plate getopt (most-specific getopt [:key-style] cluster %)))
+            (single-plate getopt cluster %))
          (derived getopt cluster :key-coordinates))))
 
 (defn cluster-cutouts [getopt cluster]

commit 2b6b8221d6110d409378cbe0aad545f71d55473e

Fixed bottom plates drawn from walls.
Fixed a regression resulting from 1c2d6938e452b68150742a5a49200331df1227b9.

src/dactyl_keyboard/cad/place.clj
@@ -196,8 +196,9 @@
       ;; Compute a 3D offset from the center of a web post to a vertex on it.
       (matrix/cube-vertex-offset
         side
-        (most-specific getopt [:wall :thickness] cluster coordinates
-          (compass/short-to-long (compass/convert-to-cardinal side)))
+        (map #(/ % 2)
+             (most-specific getopt [:wall :thickness] cluster coordinates
+               (compass/short-to-long (compass/convert-to-cardinal side))))
         keyopts)
       [0 0 0])))
 