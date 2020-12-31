thecarp/dactyl-keyboard

commit a933ab9e6148dd291c485ab174c41f0a2abe2434

Refactored configuration parser.
More conventional intermediate data structure, slightly more elegant
raws, slightly better output.

src/dactyl_keyboard/param/base.clj
@@ -13,27 +13,44 @@
 ;; Internal ;;
 ;;;;;;;;;;;;;;
 
+(spec/def ::default any?)
+(spec/def ::freely-keyed boolean?)
+(spec/def ::heading-template string?)
+(spec/def ::help string?)
+(spec/def ::leaf boolean?)
+(spec/def ::parse-fn fn?)
+(spec/def ::path (spec/coll-of keyword?))
+(spec/def ::resolve-fn fn?)
+(spec/def ::validate (spec/coll-of some?))  ; Anything spec can check against.
 
-;; TODO: Expand spec with meaningful field-specific checks.
-(spec/def ::descriptor  ; Parameter metadata descriptor.
-  #{:path :heading-template :help :default :parse-fn :validate :resolve-fn})
-(spec/def ::parameter-spec (spec/map-of ::descriptor any?))
+(spec/def ::raw  ; Bare metadata as given in configuration specifications.
+  (spec/keys :opt-un [::default ::freely-keyed ::heading-template ::parse-fn
+                      ::validate]))
+(spec/def ::metadata  ; Full metadata with interpolations.
+  (spec/and ::raw (spec/keys :req-un [::path ::help ::leaf])))
+
+(defn- split-tail
+  "Handle an optional map of metadata followed by optional docstrings."
+  [data]
+  (if (-> data first string?) [{} data] [(first data) (rest data)]))
 
 (defn- coalesce
   "Assemble one branch in a tree structure from flat specifications.
   Keep paths around for ease of building tables of content."
-  [coll [type path & metadata]]
-  (case type
-    :section
-      (assoc-in coll path
-        (ordered-map :metadata
-          {:path path, :help (apply str metadata)}))
-    :parameter
-      (assoc-in coll path
-        (merge (first metadata)
-               {:path path, :help (apply str (rest metadata))}))
-    (throw (Exception.
-             (format "Bad type in ‘%s’ configuration master." type)))))
+  [coll [path & other]]
+  (let [[metadata helptext] (split-tail other)]
+    (when-not (spec/valid? ::raw metadata)
+      (throw (ex-info "Invalid configuration metadata"
+                      {:type :internal-error
+                       :path path
+                       :raw-metadata metadata
+                       :spec-explanation (spec/explain-str ::raw metadata)})))
+    (->> metadata
+      (merge {:leaf (contains? metadata :default)
+              :path path
+              :help (apply str helptext)})
+      (assoc (ordered-map) ::metadata)
+      (assoc-in coll path))))
 
 (defn inclusive-or
   "A merge strategy for configuration keys. Take everything.
@@ -43,17 +60,8 @@
 
 (defn- explicit-only
   "A merge strategy for configuration keys. Take only what the user provides."
-  [nominal candidate]
+  [_ candidate]
   (keys candidate))
-
-(defn- leaf? [node] (spec/valid? ::parameter-spec node))
-
-(defn- hard-defaults
-  "Pick a user-supplied value over a default value.
-  This is the default method for resolving overlap between built-in defaults
-  and the user configuration, at the leaf level."
-  [nominal candidate]
-  (or candidate (:default nominal)))
 
 (defn- expand-exception
   [exception key]
@@ -74,31 +82,65 @@
 ;; Interface ;;
 ;;;;;;;;;;;;;;;
 
+(defn hard-defaults
+  "Pick a user-supplied value over a default value.
+  This is the default method for resolving overlap between built-in defaults
+  and the user configuration, at the leaf level."
+  [{:keys [default]} candidate]
+  (or candidate default))
+
 (defn soft-defaults
   "Prioritize a user-supplied value over a default value, but make it spongy.
   This is an alternate method for resolving overlap, intended for use with
   defaults that are so complicated the user will not want to write a complete,
   explicit replacement every time."
-  [nominal candidate]
-  (soft-merge (:default nominal) candidate))
+  [{:keys [default]} candidate]
+  (soft-merge default candidate))
 
 (defn inflate
   "Recursively assemble a tree from flat specifications.
-  Skip the first entry (an introduction)."
+  Skip the first entry, assuming it’s documentation."
   [flat]
   (reduce coalesce (ordered-map) (rest flat)))
+
+(defn traverse-node
+  "Treat a branch or leaf. Raise an exception on superfluous entries.
+  Branches and leaves are distinguished by arity."
+  ([leafer key-picker nominal candidate]
+   (when-not (map? candidate)
+     (throw (ex-info "Non-mapping section in configuration file"
+                     {:type :structural-error
+                      :raw-value candidate})))
+   (reduce
+     (fn [coll key]
+       (let [metadata (get-in nominal [key ::metadata])]
+         (assert (spec/valid? ::metadata metadata))
+         (assoc coll key
+           (if (:leaf metadata)
+             ;; Entry is a leaf.
+             (traverse-node leafer key-picker nominal candidate key)
+             ;; Else entry is a branch.
+             (traverse-node leafer key-picker (key nominal) (key candidate {}))))))
+     candidate
+     (remove #(= ::metadata %) (distinct (key-picker nominal candidate)))))
+  ([leafer _ nominal candidate key]
+   (when-not (contains? nominal key)
+     (throw (ex-info "Superfluous configuration key"
+                     {:type :superfluous-key
+                      :keys (list key)
+                      :accepted-keys (keys nominal)})))
+   (expand-any-exception key (leafer (key nominal) (key candidate)))))
 
 
 ;; Parsing:
 
 (defn parse-leaf
-  "Resolve differences between default values and user-supplied values.
+  "Resolve differences between settings from different sources.
   Run the result through a specified parsing function and return it."
-  [{:keys [parse-fn resolve-fn] :as nominal
-    :or {parse-fn identity, resolve-fn hard-defaults}}
-   candidate]
-  {:pre [(leaf? nominal)]}
-  (let [merged (resolve-fn nominal candidate)]
+  [{::keys [metadata]} candidate]
+  (let [{:keys [parse-fn resolve-fn]
+         :or {parse-fn identity, resolve-fn hard-defaults}} metadata
+        merged (resolve-fn metadata candidate)]
     (try
       (parse-fn merged)
       (catch Exception e
@@ -109,72 +151,31 @@
                          :parser parse-fn
                          :original-exception e}))))))
 
-(declare parse-node)
-
-(defn parse-branch
-  "Parse a section of a configuration received through the UI."
-  [key-picker nominal candidate]
-  (if-not (map? candidate)
-    (throw (ex-info "Non-mapping section in configuration file"
-                    {:type :structural-error
-                     :raw-value candidate})))
-  (reduce
-    (partial parse-node key-picker nominal)
-    candidate
-    (remove #(= :metadata %) (distinct (key-picker nominal candidate)))))
-
-(defn parse-node
-  "Parse a branch or leaf. Raise an exception on superfluous entries."
-  [key-picker nominal candidate key]
-  {:pre [(not (spec/valid? ::descriptor key))]}
-  (if (contains? nominal key)
-    (expand-any-exception key
-      (assoc candidate key
-        (if (leaf? (key nominal))
-          (parse-leaf (key nominal) (key candidate))
-          (parse-branch key-picker (key nominal) (get candidate key {})))))
-    (throw (ex-info "Superfluous configuration key"
-                    {:type :superfluous-key
-                     :keys (list key)
-                     :accepted-keys (keys nominal)}))))
-
+(def parse-inclusively (partial traverse-node parse-leaf inclusive-or))
+(def parse-explicit (partial traverse-node parse-leaf explicit-only))
 
 ;; Validation:
-
-(declare validate-leaf validate-branch)
-
-(defn validate-node
-  "Validate a fragment of a configuration received through the UI."
-  [nominal candidate key]
-  {:pre [(not (spec/valid? ::descriptor key))]}
-  (expand-any-exception key
-    (if (leaf? (key nominal))
-      (validate-leaf (key nominal) (key candidate))
-      (validate-branch (key nominal) (key candidate)))))
-
-(defn validate-branch
-  "Validate a section of a configuration received through the UI.
-  The return value should not be used."
-  [nominal candidate]
-  (mapv #(validate-node nominal candidate %) (keys candidate)))
 
 (defn validate-leaf
   "Validate a specific parameter received through the UI.
   Side effects (exception) only. The return value should not be used."
   ;; Exposed for unit testing.
-  [nominal candidate]
-  {:pre [(leaf? nominal)]}
-  (doseq [validator (get nominal :validate [])]
-    (if-not (spec/valid? validator candidate)
-      (throw (ex-info "Value out of range"
-                      {:type :validation-error
-                       :parsed-value candidate
-                       :spec-explanation (spec/explain-str validator candidate)})))))
+  [{::keys [metadata]} candidate]
+  (let [{:keys [leaf validate] :or {validate []}} metadata]
+    (assert leaf)
+    (doseq [validator validate]
+      (if-not (spec/valid? validator candidate)
+        (throw (ex-info "Value out of range"
+                        {:type :validation-error
+                         :parsed-value candidate
+                         :spec-explanation (spec/explain-str validator candidate)}))))))
+
+(def validate-branch (partial traverse-node validate-leaf explicit-only))
 
 (defn delegated-validation
   "Make a function to delegate the validation of a branch."
   [raws]
-  (fn [candidate] (validate-branch (inflate raws) candidate)))
+  (partial validate-branch (inflate raws)))
 
 
 ;; Both/other:
@@ -183,20 +184,20 @@
   "Parse a branch and then validate it.
   Trust validation failure to raise an exception."
   [nominal candidate]
-  (let [parsed (parse-branch inclusive-or nominal candidate)]
+  (let [parsed (parse-inclusively nominal candidate)]
     (validate-branch nominal parsed)
     parsed))
 
 (defn parser-with-defaults
   "Make a function to parse a branch. Close over its raw specifications."
   [raws]
-  (fn [candidate] (parse-branch inclusive-or (inflate raws) candidate)))
+  (partial parse-inclusively (inflate raws)))
 
 (defn parser-wo-defaults
   "Make a function to parse a branch without its default values.
   This is useful for parts of the configuration that can be overridden."
   [raws]
-  (fn [candidate] (parse-branch explicit-only (inflate raws) candidate)))
+  (partial parse-explicit (inflate raws)))
 
 (defn extract-defaults
   "Fetch default values for a broad section of the configuration."

src/dactyl_keyboard/param/proc/doc.clj
@@ -7,7 +7,6 @@
 
 (ns dactyl-keyboard.param.proc.doc
   (:require [clojure.string :as string]
-            [clojure.spec.alpha :as spec]
             [dactyl-keyboard.param.base :as base]))
 
 
@@ -15,10 +14,23 @@
 ;; Internal ;;
 ;;;;;;;;;;;;;;
 
-(defn- default-doc-heading
-  "Produce a default heading template for documents."
-  [is-param]
-  (if is-param "Parameter %s" "Section %s"))
+(defn- heading-style
+  [level text]
+  (if (< level 7)  ; Markdown only supports up to h6 tags.
+    (str (string/join "" (repeat level "#")) " " text)
+    (str "###### " text " at level " level)))
+
+(defn- compose-heading
+  "Produce and fill a heading template for documents."
+  [{:keys [heading-template freely-keyed leaf] :or {freely-keyed false}} content]
+  {:post (string? %)}
+  (format
+    (cond
+      heading-template heading-template
+      freely-keyed "Freely keyed section %s"
+      leaf "Parameter %s"
+      :else "Section %s")
+    content))
 
 (defn- slugify
   "Represent a section or parameter as a unique string for HTML anchors.
@@ -40,42 +52,29 @@
   site, or GitHub repents, this will break."
   [path]
   (format "<a href=\"#user-content-%s\">`%s`</a>"
-     (slugify path) (name (last path))))
+    (slugify path) (name (last path))))
 
 (defn- print-markdown-toc
   "Print an indented list as a table of contents. Descend recursively."
   [node level]
-  (doseq [[key {:keys [path metadata] :as value}] node]
-    (let [is-param (spec/valid? ::base/parameter-spec value)]
-      (when (not (= key :metadata))
-        (println
-          (str (string/join "" (repeat level "    ")) "-")
-          (format (get value :heading-template (default-doc-heading is-param))
-                  (link-to-anchor (if is-param path (:path metadata))))))
-      (if (not is-param)
+  (doseq [[key {::base/keys [metadata] :as value}] node]
+    (when-not (= key ::base/metadata)
+      (let [{:keys [path]} metadata]
+        (println (str (string/join "" (repeat level "    ")) "-")
+                 (compose-heading metadata (link-to-anchor path)))
         (print-markdown-toc value (inc level))))))
 
 (defn- print-markdown-fragment
   "Print a description of a node in the settings structure using Markdown."
   [node level]
-  (let [heading-style
-          (fn [text]
-            (if (< level 7)  ; Markdown only supports up to h6 tags.
-              (str (string/join "" (repeat level "#")) " " text)
-              (str "###### " text " at level " level)))]
-    (doseq [[key {:keys [path help metadata] :as value}] node]
-      (let [is-param (spec/valid? ::base/parameter-spec value)]
-        (when (not (= key :metadata))
-          (println)
-          (println
-            (heading-style
-              (format
-                (get value :heading-template (default-doc-heading is-param))
-                (target-anchor (if is-param path (:path metadata))))))
-          (println)
-          (println (or (if is-param help (:help metadata)) "Undocumented.")))
-        (if (not is-param)
-          (print-markdown-fragment value (inc level)))))))
+  (doseq [[key {::base/keys [metadata] :as value}] node]
+    (let [{:keys [help leaf path]} metadata]
+      (when-not (= key ::base/metadata)
+        (println)
+        (println
+          (heading-style level (compose-heading metadata (target-anchor path))))
+        (when-not (zero? (count help)) (println) (println help))
+        (when-not leaf (print-markdown-fragment value (inc level)))))))
 
 
 ;;;;;;;;;;;;;;;

src/dactyl_keyboard/param/schema/anch.clj
@@ -33,22 +33,14 @@
 (def anchoring-raws
   "The full set of anchoring parameters, in the flat base format."
   [[]
-   [:parameter [:anchor]
-    anchor-metadata]
-   [:parameter [:side]
-    anchor-side-metadata]
-   [:parameter [:segment]
-    anchor-segment-metadata]
-   [:parameter [:intrinsic-offset]
-    anchor-3d-offset-metadata]
-   [:parameter [:intrinsic-rotation]
-    anchor-3d-angle-metadata]
-   [:parameter [:extrinsic-offset]
-    anchor-3d-offset-metadata]
-   [:parameter [:extrinsic-rotation]
-    anchor-3d-angle-metadata]
-   [:parameter [:preserve-orientation]
-    {:default false, :parse-fn boolean}]])
+   [[:anchor] anchor-metadata]
+   [[:side] anchor-side-metadata]
+   [[:segment] anchor-segment-metadata]
+   [[:intrinsic-offset] anchor-3d-offset-metadata]
+   [[:intrinsic-rotation] anchor-3d-angle-metadata]
+   [[:extrinsic-offset] anchor-3d-offset-metadata]
+   [[:extrinsic-rotation] anchor-3d-angle-metadata]
+   [[:preserve-orientation] {:default false, :parse-fn boolean}]])
 
 ;; See also salient-anchoring.
 (def parse-anchoring (base/parser-with-defaults anchoring-raws))

src/dactyl_keyboard/param/schema/arb.clj
@@ -75,7 +75,7 @@
     and leaf parsers. The initial dispatcher replaces itself with a secondary
     dispatcher each time it passes a branch node, and the secondary dispatcher
     sustains itself by the trick of its parser being a function that refers to
-    itself and thereby passes itself along by recreating the lower-lever
+    itself and thereby passes itself along by recreating the lower-level
     dispatcher on each pass.
 
     A candidate to the dispatcher can be a lazy sequence describing a single
@@ -130,5 +130,5 @@
 ;; Composite ;;
 ;;;;;;;;;;;;;;;
 
-(def parameter-metadata {:heading-template "Special section %s"
-                         :default {} :parse-fn parse-grove :validate [::map]})
+(def parameter-metadata {:freely-keyed true, :default {},
+                         :parse-fn parse-grove, :validate [::map]})

src/dactyl_keyboard/param/tree/central.clj
@@ -22,14 +22,14 @@
     "When present, a central housing naturally determines the position of each "
     "other part of the keyboard: Key clusters on each side of the "
     "main body should be anchored to points on the central housing."]
-   [:parameter [:include]
+   [[:include]
     {:default false :parse-fn boolean}
     "If this and `main-body` → `reflect` are both true, add a central housing."]
-   [:parameter [:preview]
+   [[:preview]
     {:default false :parse-fn boolean}
     "If `true`, include the central housing when rendering each half of the "
     "main body of the keyboard."]
-   [:section [:shape]
+   [[:shape]
     "The shape of the central housing determines, in part, how it connects "
     "to the rest of the keyboard, including the general shape of an optional "
     "adapter. "
@@ -45,13 +45,13 @@
     "The adapters are the gables on either side of a particular house, "
     "up against which two others house are built: The two mirrored halves of "
     "the main body."]
-   [:parameter [:shape :width]
+   [[:shape :width]
     {:default 1 :parse-fn num}
     "The approximate extent of the housing itself, on the x axis, in mm."]
-   [:parameter [:shape :thickness]
+   [[:shape :thickness]
     {:default 1 :parse-fn num}
     "The thickness of the housing and its adapter (if any), in mm."]
-   [:parameter [:shape :interface]
+   [[:shape :interface]
     {:default [] :parse-fn parse/central-housing-interface
      :validate [::valid/central-housing-interface]}
     "The `interface` setting is essentially a list of points: "
@@ -184,57 +184,57 @@
     "interface. They are intended as anchors for key clusters and tweaks, "
     "where their responsiveness to width alone comes in handy as you work out "
     "the precise shape of the housing."]
-   [:section [:adapter]
+   [[:adapter]
     "The central housing can connect to key clusters through an adapter: "
     "A part that is shaped like the central housing and extends the "
     "rest of the case to meet the central housing at an interface.\n\n"
     "Using `tweaks`, points on the adapter should be connected to key "
     "cluster walls to close the case around the adapter but leave the adapter "
     "itself open."]
-   [:parameter [:adapter :include]
+   [[:adapter :include]
     {:default false :parse-fn boolean}
     "If this is `true`, add an adapter for the central housing."]
-   [:parameter [:adapter :width]
+   [[:adapter :width]
     {:default 1 :parse-fn num}
     "The approximate width of the adapter on each side of the central housing, "
     "along its axis (the x axis). Individual points on the adapter can be "
     "offset from this width using the `interface` list."]
-   [:section [:adapter :lip]
+   [[:adapter :lip]
     "To stabilize the connection between the central housing and the adapter, "
     "the interface between them can include an interior lip."]
-   [:parameter [:adapter :lip :include]
+   [[:adapter :lip :include]
     {:default false :parse-fn boolean}
     "If `true`, attach a lip to the central housing."]
-   [:parameter [:adapter :lip :thickness]
+   [[:adapter :lip :thickness]
     {:default 1 :parse-fn num}
     "The thickness of the lip at each point along it, in mm."]
-   [:section [:adapter :lip :width]
+   [[:adapter :lip :width]
     "The lip extends in both directions from the edge of the central housing: "
     "Both into the adapter (the “outer” part) and into the housing itself "
     "(the “inner” part), where it grows out of the inner wall with an optional "
     "taper. The total width of the lip is the sum of all these sections."]
-   [:parameter [:adapter :lip :width :outer]
+   [[:adapter :lip :width :outer]
     {:default 1 :parse-fn num}
     "The distance the lip protrudes outside the central housing and thence "
     "into the adapter, in mm."]
-   [:parameter [:adapter :lip :width :inner]
+   [[:adapter :lip :width :inner]
     {:default 1 :parse-fn num}
     "The width of the lip inside the central housing, before it starts to "
     "taper, in mm."]
-   [:parameter [:adapter :lip :width :taper]
+   [[:adapter :lip :width :taper]
     {:default 0 :parse-fn num}
     "The width of a taper from the inner portion of the lip to the inner "
     "wall of the central housing, in mm.\n\n"
     "The default value, zero, produces a right-angled transition. The higher "
     "the value, the more gentle the transition becomes."]
-   [:section [:adapter :fasteners]
+   [[:adapter :fasteners]
     "To connect the central housing and the adapter, threaded fasteners "
     "can be driven through the wall of either, into receivers extending "
     "from the other."]
-   [:parameter [:adapter :fasteners :bolt-properties]
+   [[:adapter :fasteners :bolt-properties]
     stock/implicit-threaded-bolt-metadata
     stock/threaded-bolt-documentation]
-   [:parameter [:adapter :fasteners :positions]
+   [[:adapter :fasteners :positions]
     {:default []
      :parse-fn parse/central-housing-normal-positions
      :validate [::valid/central-housing-normal-positions]}
@@ -285,59 +285,59 @@
     "This feature may help resolve problems with sloping adapters or other "
     "obstacles, but consider it experimental. More detailed overrides for "
     "placement may be introduced in a future version, if they are needed."]
-   [:section [:adapter :receivers]
+   [[:adapter :receivers]
     "One receiver is created for each of the `fasteners`. Each of these "
     "has a threaded hole to keep the fastener in place. Like the adapter lip, "
     "receivers extend from the inside wall, but receivers are anchored across "
     "the interface from their respective fasteners: A positive "
     "`axial-offset`, above, extends a receiver from the central housing "
     "into the adapter."]
-   [:section [:adapter :receivers :thickness]
+   [[:adapter :receivers :thickness]
     "The thickness of material in various parts of each receiver."]
-   [:parameter [:adapter :receivers :thickness :rim]
+   [[:adapter :receivers :thickness :rim]
     {:default 1 :parse-fn num}
     "The maximum thickness of the loop of each receiver where it grabs the "
     "fastener, in mm."]
-   [:section [:adapter :receivers :thickness :bridge]
+   [[:adapter :receivers :thickness :bridge]
     "The part between the hole and the tapering end, where the receiver "
     "extends axially across the interface."]
-   [:parameter [:adapter :receivers :thickness :bridge :radial]
+   [[:adapter :receivers :thickness :bridge :radial]
     {:default 1 :parse-fn num}
     "The thickness of each receiver in the plane of the housing wall, in mm."]
-   [:parameter [:adapter :receivers :thickness :bridge :tangential]
+   [[:adapter :receivers :thickness :bridge :tangential]
     {:default 1 :parse-fn num}
     "The thickness of each receiver out from the housing wall, in mm. "
     "This is bounded by the depth of the screw hole."]
-   [:section [:adapter :receivers :width]
+   [[:adapter :receivers :width]
     "This section is analogous to lip `width`. The “outer” width of "
     "each receiver is a function of its fastener’s lateral offset and cannot "
     "be configured here."]
-   [:parameter [:adapter :receivers :width :inner]
+   [[:adapter :receivers :width :inner]
     {:default 1 :parse-fn num}
     "The width of the receiver at its base, before it starts to taper, in mm."]
-   [:parameter [:adapter :receivers :width :taper]
+   [[:adapter :receivers :width :taper]
     {:default 0 :parse-fn num}
     "The width of a taper, as with the lip."]
-   [:section [:bottom-plate]
+   [[:bottom-plate]
     "Any bottom plating for the case will extend to the midpoint of the "
     "central housing, on the assumption that bottom-plating anchors will "
     "be used to attach it there."]
-   [:section [:bottom-plate :projections]
+   [[:bottom-plate :projections]
     "To facilitate printing a central housing standing on its edge, or to add "
     "strength, you can extend bottom-plating anchors onto the nearest wall, "
     "via a convex hull of each anchor and its projection. The result is an "
     "internal chamfer resembling a primitive fillet."]
-   [:parameter [:bottom-plate :projections :include]
+   [[:bottom-plate :projections :include]
     {:default false :parse-fn boolean}
     "If `true`, extend each bottom-plating anchor."]
-   [:parameter [:bottom-plate :projections :scale]
+   [[:bottom-plate :projections :scale]
     {:default [1 1] :parse-fn vec :validate [::tarmi-core/point-2d]}
     "The scale of each projection, as a 2-tuple of horizontal and vertical "
     "factors. The horizontal factor controls the width of the projection and "
     "the vertical factor its height. The length of the projection is fixed "
     "at the distance between the center of the anchor and the outermost "
     "part of its shell."]
-   [:parameter [:bottom-plate :fastener-positions]
+   [[:bottom-plate :fastener-positions]
     {:default []
      :parse-fn (parse/tuple-of anch/parse-anchoring)
      :validate [(spec/coll-of anch/validate-anchoring)]}

src/dactyl_keyboard/param/tree/cluster.clj
@@ -20,7 +20,7 @@
     "and position of any individual cluster of keys. One set of such "
     "options will exist for each entry in `key-clusters`, a section "
     "documented [here](options-main.md)."]
-   [:parameter [:matrix-columns]
+   [[:matrix-columns]
     {:default [{}]
      :parse-fn vec
      :validate [(spec/coll-of ::valid/column-disposition)]}
@@ -57,7 +57,7 @@
     "on. On the left-hand side of a keyboard with `main-body` → `reflect`, "
     "main-body key clusters are mirrored so that `[0, 0]` will be G instead "
     "of H in QWERTY, `[1, 0]` will be F instead of J, and so on."]
-   [:parameter [:style]
+   [[:style]
     {:default :standard :parse-fn keyword :validate [::valid/cluster-style]}
     "Cluster layout style. One of:\n\n"
     "- `standard`: Both columns and rows have the same type of curvature "
@@ -65,7 +65,7 @@
     "- `orthographic`: Rows are curved somewhat differently. This creates "
     "more space between columns and may prevent key mounts from fusing "
     "together if you have a broad matrix."]
-   [:parameter [:aliases]
+   [[:aliases]
     {:default {}
      :parse-fn (parse/map-of keyword
                               (parse/tuple-of parse/keyword-or-integer))
@@ -73,7 +73,7 @@
                              (spec/coll-of ::valid/flexcoord :count 2))]}
     "A map of short names to specific keys by coordinate pair. "
     "These names can be used as anchors for other features."]
-   [:parameter [:anchoring]
+   [[:anchoring]
     anch/anchoring-metadata
     "Where to place the cluster. "
     stock/anchoring-documentation]])

commit 610df0dd4eefc1fc90b0f9487e7f0e0369c7ee6c

Fixed feedback on superfluous config.
This fixes a regression in a933ab9e6148dd291c485ab174c41f0a2abe2434.

src/dactyl_keyboard/param/base.clj
@@ -114,7 +114,11 @@
    (reduce
      (fn [coll key]
        (let [metadata (get-in nominal [key ::metadata])]
-         (assert (spec/valid? ::metadata metadata))
+         (when-not (spec/valid? ::metadata metadata)
+           (throw (ex-info "Superfluous entry in configuration file"
+                           {:type :superfluous-key
+                            :keys (list key)
+                            :accepted-keys (keys nominal)})))
          (assoc coll key
            (if (:leaf metadata)
              ;; Entry is a leaf.